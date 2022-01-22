2d08a3bfce9391808c91cbd7191f53701427d1df
Michael Sterzinger <69076772+idep-nter@users.noreply.github.com>
2022-01-22 23:40:54 +0100

    Update models.py

--- games/models.py
+++ games/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    price = models.FloatField(max_length=5)
+    price = models.FloatField(max_length=10)
     description = models.TextField(null=True)
     category = models.CharField(max_length=200)
     image_url = models.URLField()
